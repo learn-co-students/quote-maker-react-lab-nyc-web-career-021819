ce03516ebe1606f1b9e4dd810f6b97708546cdba
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-12 18:12:07 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,41 +1,40 @@
-import uuid from 'uuid';
-
 export default (state = [], action) => {
-  let updatedQuotes;
-  switch(action.type){
-   case "ADD_QUOTE":
-    return [...state, action.quote]
+  let quote;
 
-  case "REMOVE_QUOTE":
-    let filteredQuotes = state.filter((quote)=> {
-      return quote.id !== action.quoteId
-    })
-    return filteredQuotes
+  switch (action.type) {
 
-  case "UPVOTE_QUOTE":
-     updatedQuotes = state.map((quote)=> {
-      if (quote.id === action.quoteId){
-        return {...quote, votes: ++quote.votes}
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      let updatedQuotes = state.map(quote => {
+        if (quote.id === action.quoteId) {
+          return {...quote, votes: ++quote.votes}
+        } else {
+          return quote
+        }
+      })
+      return updatedQuotes
+
+    case 'DOWNVOTE_QUOTE':
+      quote = state.find(quote => quote.id === action.quoteId)
+      if (quote.votes === 0) {
+        return state
+      } else {
+        let updatedQuotes = state.map(quote => {
+          if (quote.id === action.quoteId) {
+            return { ...quote, votes: --quote.votes}
+          } else {
+            return quote
+          }
+        })
+        return updatedQuotes
       }
-      else {
-        return quote
-      }
-    })
-    return updatedQuotes
 
-  case "DOWNVOTE_QUOTE":
-    updatedQuotes = state.map((quote)=> {
-    if(quote.id === action.quoteId && quote.votes > 0){
-      return {...quote, votes: --quote.votes}
-    }
-    else {
-      return quote
-    }
-  })
-  return updatedQuotes
-
-
-   default:
-   return state;
+    default:
+      return state;
   }
 }
