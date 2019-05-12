3105d5dd255bb2e9bb8a632bbe6f6a0f8a053a13
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-12 17:28:21 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,41 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  let updatedQuotes;
+  switch(action.type){
+   case "ADD_QUOTE":
+    return [...state, action.quote]
+
+  case "REMOVE_QUOTE":
+    let filteredQuotes = state.filter((quote)=> {
+      return quote.id !== action.quoteId
+    })
+    return filteredQuotes
+
+  case "UPVOTE_QUOTE":
+     updatedQuotes = state.map((quote)=> {
+      if (quote.id === action.quoteId){
+        return {...quote, votes: ++quote.votes}
+      }
+      else {
+        return quote
+      }
+    })
+    return updatedQuotes
+
+  case "DOWNVOTE_QUOTE":
+    updatedQuotes = state.map((quote)=> {
+    if(quote.id === action.quoteId && quote.votes > 0){
+      return {...quote, votes: --quote.votes}
+    }
+    else {
+      return quote
+    }
+  })
+  return updatedQuotes
+
+
+   default:
+   return state;
+  }
 }
